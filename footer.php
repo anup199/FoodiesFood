89da071d79d95d1f5872a4cc2ad1281e42275f5a
Anup Singh <72704047+anup199@users.noreply.github.com>
2024-06-23 09:49:52 +0530

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,10 +1,28 @@
-   <!-- footer section starts -->
-          <div class="footer text-center">
-               <div class="wrapper">
-                    <p>2023 All Rights Reserved, Some Restaurant By - <a href="#">Anup Singh</a></p>
-               </div>
+ <!-- social Section Starts Here -->
+    <section class="social">
+        <div class="container text-center">
+            <ul>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/50/000000/facebook-new.png"/></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/twitter.png"/></a>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <!-- social Section Ends Here -->
 
-          </div>
-          <!-- footer section ends -->          
-     </body>
+    <!-- footer Section Starts Here -->
+    <section class="footer">
+        <div class="container text-center">
+            <p>All rights reserved. Designed By <a href="#">Anup Singh</a></p>
+        </div>
+    </section>
+    <!-- footer Section Ends Here -->
+
+</body>
 </html>
