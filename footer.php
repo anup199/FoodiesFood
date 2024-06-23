0c5c1275af06b5406eda59ef0379f5a9e70a5f64
Anup Singh <72704047+anup199@users.noreply.github.com>
2024-06-23 09:41:19 +0530

    admin/partials

---
+++ footer.php
@@ -0,0 +1,10 @@
+   <!-- footer section starts -->
+          <div class="footer text-center">
+               <div class="wrapper">
+                    <p>2023 All Rights Reserved, Some Restaurant By - <a href="#">Anup Singh</a></p>
+               </div>
+
+          </div>
+          <!-- footer section ends -->          
+     </body>
+</html>
